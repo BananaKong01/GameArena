11067c4b97ab25679535af2860d1c1dc6a6298e5
Alex Barber <a.j.barber@lancaster.ac.uk>
2026-02-19 16:36:29 +0000

    Added Sample code as example code.

--- Sample.java
+++ Sample.java
@@ -1,1 +1,16 @@
+// EXAMPLE CODE
+public class Sample
+{
+    public static void main(String[] args)
+    {
+        GameArena arena = new GameArena(500, 300);
+        Ball b = new Ball(250, 150, 20, "GREEN");
 
+        arena.addBall(b);
+
+        while(true)
+        {
+            arena.pause();
+        }
+    }
+} 
