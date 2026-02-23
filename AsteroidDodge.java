df3fb908f7209e925a39f0a1436f51f430615ac3
BananaKong01 <a.j.barber@lancaster.ac.uk>
2026-02-23 14:24:10 +0000

    Singular rectangle now works :)

--- AsteroidDodge.java
+++ AsteroidDodge.java
@@ -1,12 +1,10 @@
-public class AsteroidDodge {
-    // Create arena and characters
-    GameArena arena = new GameArena(500, 300);
-    Spaceship spaceship = new Spaceship(250, 150);
-
+public class AsteroidDodge { 
     public static void main(String[] args) {
+        // Create arena and characters
+        GameArena arena = new GameArena(500, 300);
+        Spaceship spaceship = new Spaceship(250, 150);
 
-
-
+        spaceship.addTo(arena);
 
         while(true) {
             arena.pause(); // Stops from dropping out of main
